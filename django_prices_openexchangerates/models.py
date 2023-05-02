f4176b82b2cf981b3942d99ac2b558ee605a1481
zebra-lucky <zebra.lucky@gmail.com>
2023-05-02 05:04:01 +0300

    replace ugettext_lazy with gettext_lazy

--- django_prices_openexchangerates/models.py
+++ django_prices_openexchangerates/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.cache import cache
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .currencies import CURRENCIES
 
